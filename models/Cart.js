39c2e8acbe8d30965a608aa9418b2f98948389de
jansencodez <nyamwecliff@gmail.com>
2024-10-26 21:22:01 +0300

    schema update

--- models/Cart.js
+++ models/Cart.js
@@ -1,11 +1,25 @@
 const mongoose = require("mongoose");
 
-const cartSchema = new mongoose.Schema({
-  userId: { type: mongoose.Types.ObjectId, ref: "User", required: true },
-  Items: {
-    productId: { type: mongoose.Types.ObjectId, ref: "Product" },
-    quantity: { type: Number, required: true, default: 1 },
+const CartSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
   },
+  items: [
+    {
+      productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+        default: 1,
+      },
+    },
+  ],
 });
 
-module.exports = mongoose.model("Cart", cartSchema);
+module.exports = mongoose.model("Cart", CartSchema);
