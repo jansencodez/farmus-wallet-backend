766dde30e721b6443c84de6cb353bdfbf6b3eb68
jansencodez <nyamwecliff@gmail.com>
2024-10-26 20:48:15 +0300

    minor fix

--- api/cart/add/index.js
+++ api/cart/add/index.js
@@ -1,4 +1,3 @@
-// api/cart/[userId]/add.js
 import connectDB from "../../../config/db";
 import Cart from "../../../models/Cart";
 
@@ -10,11 +9,16 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
       const { productId, quantity } = req.body;
+
       const cart = await Cart.findOneAndUpdate(
         { userId },
-        { $addToSet: { items: { productId, quantity } } },
+        {
+          $addToSet: { items: { productId, quantity } },
+          $setOnInsert: { userId }, // Set userId only if the document is new
+        },
         { new: true, upsert: true }
       );
+
       res.status(201).json(cart);
     } catch (error) {
       res.status(500).json({ message: "Error adding item to cart", error });
