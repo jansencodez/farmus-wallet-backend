7ec053dc6418bafb77a7b11defc749a704041828
jansencodez <nyamwecliff@gmail.com>
2024-10-27 00:48:13 +0300

    schema ref fix

--- api/cart/index.js
+++ api/cart/index.js
@@ -1,6 +1,8 @@
 // api/cart/[userId].js
 import connectDB from "../../config/db";
 import { Cart } from "../../models/user";
+import Product from "../../models/Product";
+import mongoose from "mongoose";
 
 export default async function handler(req, res) {
   const userId = req.query.id;
@@ -15,13 +17,25 @@ export default async function handler(req, res) {
 
   if (req.method === "GET") {
     try {
-      const cart = await Cart.findOne({ userId }).populate("items.productId");
-      res.status(200).json({ success: true, items: cart ? cart.items : [] });
+      // Convert userId to ObjectId
+      const cart = await Cart.findOne({
+        userId: mongoose.Types.ObjectId(userId),
+      }).populate("items.productId");
+
+      // Check if the cart was found
+      if (!cart) {
+        return res
+          .status(404)
+          .json({ success: false, message: "Cart not found" });
+      }
+
+      const { items } = cart; // Destructure items for response
+      res.status(200).json({ success: true, items });
     } catch (error) {
-      console.error("Error retrieving cart:", error);
+      console.error("Error retrieving cart:", error.message); // Log only the error message
       res
         .status(500)
-        .json({ success: false, message: "Error retrieving cart", error });
+        .json({ success: false, message: "Error retrieving cart" });
     }
   } else {
     res.setHeader("Allow", ["GET"]);
